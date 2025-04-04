35c3f5cac78704563740bccd611fdba69ef59c45
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-04-04 11:28:52 -0500

    04.04.25

---
+++ src/test/java/remain/JavaDataStructures/JavaLinkedList.java
@@ -0,0 +1,16 @@
+package remain.JavaDataStructures;
+import java.util.LinkedList;
+
+
+public class JavaLinkedList {
+    public static void main(String [] args){
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+
+    }
+
+}

b4ef36a2b38c7183b1bdb767347ba220a7683961
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-04-07 16:30:03 -0500

    04.07.25 1

---
+++ src/test/java/remain/JavaDataStructures/JavaHashSet.java
@@ -0,0 +1,35 @@
+package remain.JavaDataStructures;
+
+import java.util.HashSet;
+
+public class JavaHashSet {
+    public static void main(String [] args){
+        HashSet<String> cars = new HashSet<String>();
+        HashSet<Integer> numbers = new HashSet<Integer>();
+
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("BMW");
+        cars.add("Mazda");
+        System.out.println(cars);
+        System.out.println(cars.contains("Mazda"));
+        System.out.println(cars.size());
+        for(String i : cars){
+            System.out.println(i);
+        }
+
+        numbers.add(4);
+        numbers.add(7);
+        numbers.add(8);
+
+        for(int i = 1; i <= 10; i++){
+            if(numbers.contains(i)){
+                System.out.println(i + " was found in the set.");
+            }else{
+                System.out.println(i + "was not found in the set.");
+            }
+        }
+
+    }
+}

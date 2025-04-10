a2f7871aac51ec1626aed117c4d7fe51a9cad7fd
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-04-10 14:21:48 -0500

    04.10.25

---
+++ src/test/java/remain/JAVA/JavaAdvanced/JavaRegEx.java
@@ -0,0 +1,17 @@
+package remain.JAVA.JavaAdvanced;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JavaRegEx {
+    public static void main(String [] args){
+        Pattern pattern = Pattern.compile("Erlan", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher("Abduraimov Erlan");
+        boolean matchFound = matcher.find();
+        if(matchFound){
+            System.out.println("Match Found");
+        }else{
+            System.out.println("match not Found");
+        }
+    }
+}

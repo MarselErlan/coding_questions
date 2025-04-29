3ff192c209a8381136d4ed2262cb51a810b85c24
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-04-29 03:03:49 -0500

    04.29.25 1

--- src/test/java/remain/JAVA/JavaAdvanced/JavaAdvancedSorting.java
+++ src/test/java/remain/JAVA/JavaAdvanced/JavaAdvancedSorting.java
@@ -32,6 +32,28 @@ class SortByYear implements Comparator{
 }
 
 
+class SortEvenFirst implements Comparator{
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        Integer a = (Integer) o1;
+        Integer b = (Integer) o2;
+
+        boolean aIsEven = (a % 2) == 0;
+        boolean bIsEven = (b % 2) == 0;
+
+        if(aIsEven == bIsEven){
+            if(aIsEven){
+                return -1;
+            }else {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
+
+
 public class JavaAdvancedSorting {
     public static void main(String [] args){
         ArrayList<Car> myCars = new ArrayList<Car>();
@@ -39,8 +61,25 @@ public class JavaAdvancedSorting {
         myCars.add(new Car("Honda", "Accord", 2006));
         myCars.add(new Car("Ford", "Mustang", 1970));
 
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+        myNumbers.add(33);
+        myNumbers.add(15);
+        myNumbers.add(20);
+        myNumbers.add(34);
+        myNumbers.add(8);
+        myNumbers.add(12);
+
+        Comparator myComparator2 = new SortEvenFirst();
+        Collections.sort(myNumbers, myComparator2);
+
         Comparator myComparator = new SortByYear();
         Collections.sort(myCars, myComparator);
+
+        for(int i: myNumbers){
+            System.out.println(i);
+        }
+
+        System.out.println("-----------------------");
 
         for(Car c: myCars){
             System.out.println(c.brand + " " + c.model + " " + c.year);

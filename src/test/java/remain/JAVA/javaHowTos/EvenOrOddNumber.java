7fae708a95a1ddc39c4aa3bcff8c8268ec0c4003
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-05-05 22:46:52 -0500

    05.05.25

---
+++ src/test/java/remain/JAVA/javaHowTos/EvenOrOddNumber.java
@@ -0,0 +1,14 @@
+package remain.JAVA.javaHowTos;
+
+public class EvenOrOddNumber {
+    public static void main(String [] args){
+        int number = 5;
+
+        if(number % 2 == 0){
+            System.out.println(number + "is even.");
+        }else{
+            System.out.println(number + "is odd.");
+        }
+
+    }
+}

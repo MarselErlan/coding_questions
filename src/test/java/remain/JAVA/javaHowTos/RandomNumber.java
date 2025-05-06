25ebe5311378451d4d3093ec8f45f8ae2a52a934
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-05-05 22:53:18 -0500

    05.05.25 1

---
+++ src/test/java/remain/JAVA/javaHowTos/RandomNumber.java
@@ -0,0 +1,10 @@
+package remain.JAVA.javaHowTos;
+
+public class RandomNumber {
+    public static void main(String []args){
+        System.out.println(Math.random());
+        int randomNum = (int)(Math.random() * 101);
+        System.out.println(randomNum);
+
+    }
+}
